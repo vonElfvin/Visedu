4d7e24c5b5e6d50a9947e094f86ef96fef0ccf47
vonElfvin <elvin.granat@gmail.com>
2018-03-23 23:13:08 +0100

    Updated app component to show navbar and footer

--- Frontend/src/app/core/core.module.ts
+++ Frontend/src/app/core/core.module.ts
@@ -1,15 +1,27 @@
 // angular modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+// material modules
+import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material';
 
 // components
 import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
+    // angular modules
     CommonModule,
+    HttpClientModule,
+
+    // material modules
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
   ],
-  declarations: [NavComponent],
-  exports: [NavComponent]
+  declarations: [NavComponent, FooterComponent],
+  exports: [NavComponent, FooterComponent]
 })
 export class CoreModule { }
