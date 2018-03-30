71ad7fa37718e250d1172eab1c17e6128a329159
vonElfvin <elvin.granat@gmail.com>
2018-03-30 19:12:43 +0200

    Deployed to Firebase Hosting

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBcsOkV4SdWeeYQvz6pnB2Z6w3qqWoiNi8',
+    authDomain: 'visedu-app.firebaseapp.com',
+    databaseURL: 'https://visedu-app.firebaseio.com',
+    projectId: 'visedu-app',
+    storageBucket: 'visedu-app.appspot.com',
+    messagingSenderId: '1002636738243'
+  }
 };
